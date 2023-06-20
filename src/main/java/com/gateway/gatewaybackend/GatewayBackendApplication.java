42fad4dbcc7c1d2f2c46cf73edbf8a3abc320a02
ymartmengual <ymartmengual@gmail.com>
2023-06-20 03:53:34 -0400

    commit initial

---
+++ src/main/java/com/gateway/gatewaybackend/GatewayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.gatewaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gateway/gatewaybackend/GatewayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.gatewaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
